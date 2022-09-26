15732dd347bc68da7b822e94dae42d94e9ce07fd
adski13 <71898448+adski13@users.noreply.github.com>
2022-09-26 10:09:48 +0300

    10:09 26.09.2022

---
+++ TRPO/Models/Passanger.cs
@@ -0,0 +1,14 @@
+﻿namespace TRPO.Models
+{
+    public class Passanger
+    {
+        public int Passanger_id { set; get; }
+        public string Name { set; get; }
+        public string Surname { set; get; }
+        public string Passport_ser { set; get; }
+        public DateOnly Date_of_birth { set; get; }
+        public string Telephone { set; get; }
+        public string Password { set; get; }
+    }
+    
+}
