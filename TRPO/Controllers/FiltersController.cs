1c95d6459a1ae280112a87b521d70fe694dc8bd5
adski13 <71898448+adski13@users.noreply.github.com>
2022-10-04 00:20:06 +0300

    04.10.2022 0:20

--- TRPO/Controllers/FiltersController.cs
+++ TRPO/Controllers/FiltersController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using TRPO.Models;
 
 namespace TRPO.Controllers
 {
@@ -8,5 +11,53 @@ namespace TRPO.Controllers
         {
             return View();
         }
+
+        public IActionResult Check()
+        {
+            string finishPoint = Request.Form["FinishPoint"];
+            DataBase dataBase = new DataBase();
+            var connection = dataBase.GetConnection();
+            FiltersBuilder filtersBuilder = new FiltersBuilder(connection);
+            filtersBuilder.SetFinishPoint(finishPoint);
+            var command = filtersBuilder.GetResault();
+            dataBase.openConnection();
+            SqlDataReader reader = command.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader.GetInt32(0));
+                    Console.WriteLine(DateOnly.FromDateTime(reader.GetDateTime(1)));
+                    Console.WriteLine(TimeOnly.FromDateTime(reader.GetDateTime(2))); 
+                    //TimeOnly finishTime = TimeOnly.FromDateTime(reader.GetDateTime(3));
+                    //string startPoint = reader.GetString(4);
+                    //string finishPoint = reader.GetString(5);
+                    //int planeId = reader.GetInt32(6);
+                    //int crewId = reader.GetInt32(7);
+                    //tmpFlights.Add(new Flight(id, date, startTime, finishTime, startPoint, finishPoint, planeId, crewId));
+                }
+
+            }
+            dataBase.closeConnection();
+            //DataTable table = new DataTable();
+            //SqlDataAdapter adapter = new SqlDataAdapter();
+            //adapter.SelectCommand = command;
+            ////adapter.Fill(table);
+            //DataRow[] userInfo;
+            //userInfo = table.Select();
+            //if (userInfo.Length > 0)
+            //{
+            //    Console.WriteLine("Вы вошли");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Неправильный логин и пароль");
+            //}
+            //Console.WriteLine(priceFrom);
+
+            return RedirectToAction("Index");
+        }
+
     }
 }
---
+++ TRPO/FiltersBuilder.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Data.SqlClient;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace TRPO
+{
+    public class FiltersBuilder
+    {
+        private string _commandExpression;
+        private string _filters;
+        private readonly SqlConnection _connection;
+        private SqlCommand command = new SqlCommand();
+        public FiltersBuilder(SqlConnection connection)
+        {
+            _connection = connection;
+            _commandExpression = $"SELECT * FROM Flight ";
+        }
+        private bool addCondition()
+        {
+            if (_filters == ""||_filters == " "||_filters==null)
+            {
+                _filters += "WHERE ";
+                return true;
+            }
+            else
+            {
+                _filters += "AND ";
+            }
+            return false;
+        }
+
+        //public void SetPriceFrom(string priceFrom)
+        //{
+        //    addCondition();
+        //    command.Parameters.Add("@priceFrom", System.Data.SqlDbType.NChar).Value = priceFrom;
+        //    _filters += $"PRICE >= @priceFrom";    
+        //}
+        //public void SetPriceTo(string priceTo)
+        //{
+        //    addCondition();
+        //    command.Parameters.Add("@priceTo", System.Data.SqlDbType.NChar).Value = priceTo;
+        //    _filters += $"PRICE <= @priceFrom";
+
+
+        //}
+        //public void SetDateFrom(DateTime DateFrom)
+        //{
+        //    addCondition();
+        //    command.Parameters.Add("@priceTo", System.Data.SqlDbType.DateTime).Value = DateFrom;
+        //    _filters += $"PRICE <= @priceFrom";
+        //}
+        //public void SetDateTo(string DateTo)
+        //{
+        //    addCondition();
+        //    command.Parameters.Add("@priceTo", System.Data.SqlDbType.DateTime).Value = DateFrom;
+        //    _filters += $"PRICE <= @priceFrom";
+
+        //}
+        public void SetFinishPoint(string finishPoint)
+        {
+            addCondition();
+            
+            _filters += $"Finish_point = @finishPoint";
+            command.Parameters.Add("@finishPoint", System.Data.SqlDbType.NChar, 20).Value = finishPoint;
+        }
+
+
+        
+
+        public SqlCommand GetResault()
+        {
+            _commandExpression += _filters;
+            command.CommandText = _commandExpression;
+            command.Connection = _connection;
+            return command;
+        }
+    }
+}
